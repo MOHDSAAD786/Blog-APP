d7f64f1bbe4d925aabf28a318654ea5b9c62d368
MOHD SAAD <011mohdsaad2022cse@gmail.com>
2025-07-12 16:11:04 +0530

    Update Login.jsx

--- Assignment/frontend/src/pages/Login.jsx
+++ Assignment/frontend/src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     console.log(input);
 
     try {
-      const response = await axios.post(`http://localhost:5100/api/v1/user/login`, input, {
+      const response = await axios.post(`https://blog-app-ncar.onrender.com/api/v1/user/login`, input, {
         headers: {
           "Content-Type": "application/json"
         },
